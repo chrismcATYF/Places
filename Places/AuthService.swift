a77b5d1c09e4d1d4e553cb789c7b18cd9f9784ae
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-09-14 21:06:31 -0500

    More UI elements added

---
+++ Places/AuthService.swift
@@ -0,0 +1,16 @@
+//
+//  AuthService.swift
+//  Places
+//
+//  Created by Chris McDonald on 9/14/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import UIKit
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    
+}
