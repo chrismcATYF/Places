2c037ff304ea95c82ce5e36f4dc1b7d892d79b80
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-09-14 21:11:47 -0500

    Remade changes

--- Places/UserDataService.swift
+++ Places/UserDataService.swift
@@ -35,7 +35,21 @@ class UserDataService {
         let comma = CharacterSet(charactersIn: ",")
         scanner.charactersToBeSkipped = skipped
         var r, g, b, a: NSString?
-        
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        let defaultColor = UIColor.lightGray
+        guard let rUnwrapped = r else { return defaultColor }
+        guard let gUnwrapped = g else { return defaultColor }
+        guard let bUnwrapped = b else { return defaultColor }
+        guard let aUnwrapped = a else { return defaultColor }
+        let rFloat = CGFloat(rUnwrapped.doubleValue)
+        let gFloat = CGFloat(gUnwrapped.doubleValue)
+        let bFloat = CGFloat(bUnwrapped.doubleValue)
+        let aFloat = CGFloat(aUnwrapped.doubleValue)
+        let newUIColor = UIColor(red: rFloat, green: gFloat, blue: bFloat, alpha: aFloat)
+        return newUIColor
     }
     
     func logoutUser() {
